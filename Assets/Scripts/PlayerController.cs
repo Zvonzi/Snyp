f41494684df7b007bf6c21b1349e6648ba2cb7e7
Zvonzi <zgadze@gmail.com>
2020-04-14 21:04:10 +0200

    Ver. 140420

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Rigidbody2D playerRB;
+    [SerializeField] float playerSpeed = 2f;
+    Vector2 movement;
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("moveX", movement.x);
+        animator.SetFloat("moveY", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        playerRB.MovePosition(playerRB.position + movement * playerSpeed * Time.fixedDeltaTime);
+    }
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# Snyp
-Projekt SNYP
